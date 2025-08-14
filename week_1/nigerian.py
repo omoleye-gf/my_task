77c3bcce3f05a9ee0fb2c5529d92c7c19ef3cb45
Oyinkansola <Oyinkansolapelumi9@gmail.com>
2025-08-14 13:22:11 +0100

    Week 1

---
+++ week_1/nigerian.py
@@ -0,0 +1,4 @@
+festival_name =input("tell us your festival name")
+location = input("tell us the location of your festival")
+month = input ("tell us the month it was held")
+print("{festival_name}")
